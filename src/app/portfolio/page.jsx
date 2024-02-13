830ab61ce4fd875a57e40fc867ca3aa805e3cb21
junddao <y2hunter@gmail.com>
2024-02-13 18:01:16 +0900

    portfolio ing...

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const Page = () => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll(ref);
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <motion.div
       className="h-full"
@@ -10,7 +17,12 @@ const Page = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div>portfolio</div>
+      <div className="h-[600vh]" ref={ref}>
+        <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl">
+          My Work
+        </div>
+        <div className="sticky top-0 flex h-screen gap-4 items-center"></div>
+      </div>
     </motion.div>
   );
 };
